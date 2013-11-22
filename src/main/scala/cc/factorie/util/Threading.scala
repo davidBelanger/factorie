3b6040bb1339d2f7a83d444cc3986275d0495571
David Belanger <david.b.belanger@gmail.com>
2013-11-22 12:02:04 -0500

    adding ProducerConsumer Iterator

--- src/main/scala/cc/factorie/util/Threading.scala
+++ src/main/scala/cc/factorie/util/Threading.scala
@@ -1,6 +1,10 @@
 package cc.factorie.util
 
 import java.util.concurrent.{Executors, Callable, ExecutorService}
+import akka.actor.{Actor, Props, ActorSystem}
+import concurrent.duration.Duration
+import concurrent.{Future, Await}
+import akka.actor.Status.Success
 
 /**
  * User: apassos
@@ -39,3 +43,44 @@ object Threading {
     } finally pool.shutdown()
   }
 }
+
+object ProducerConsumerProcessing{
+  import akka.pattern.ask
+
+  object IteratorMutex
+  def parForeach[In](xs: Iterator[In], numParallelJobs: Int = Runtime.getRuntime.availableProcessors(),perJobTimeout: Long = 10 ,overallTimeout: Long = 24)(body: In => Unit): Unit  = {
+    val system = ActorSystem("producer-consumer")
+
+    val actors = (0 until numParallelJobs).map(i => system.actorOf(Props(new ParForeachActor(body)), "actor-"+i))
+
+
+    val futures = actors.map(a => a.ask(Message(xs))(Duration(perJobTimeout,"minutes")))
+    Await.result(Future.sequence(futures.toSeq), Duration(overallTimeout,"hours"))
+    system.shutdown()
+
+  }
+
+  class ParForeachActor[In](function: In => Unit) extends Actor {
+    def getNext(a: Iterator[In]): Option[In] = {
+      IteratorMutex synchronized {
+        if (a.hasNext)
+          Some(a.next())
+        else
+          None
+      }
+    }
+    def receive = {
+      case Message(a) => {
+        var stillWorking = true
+        while (stillWorking) {
+          val next = getNext(a.asInstanceOf[Iterator[In]])
+          if(next.isDefined) function(next.get) else stillWorking = false
+        }
+        sender ! Success
+      }
+    }
+  }
+  case class Message[T](a: Iterator[T])
+
+}
+
